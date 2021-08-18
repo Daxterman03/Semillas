78148e91c97beef8234899c5f90886f598c4ef83
dedit8 <64446967+dedit8@users.noreply.github.com>
2021-08-18 20:31:29 -0300

    Add files via upload

--- semi/home.php
+++ semi/home.php
@@ -1,12 +1,27 @@
 <?php
+require 'conexion.php';
 require 'navbaru.php';
 session_start();
 $dni = $_SESSION['dni'];
+$sql="SELECT * from usu1 where dni='$dni'";
+$result=mysqli_query($conexion,$sql);
+while($mostrar=mysqli_fetch_assoc($result)) {
+
 if(!isset($dni)){
     header("location: index");
 } else {
-  echo "<h1>BIENVENIDO $dni </h1>";
-}
+  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>bienvenido   <?php echo $mostrar['nombre']; ?></h1>
+  </body>
+</html>
 
 
- ?>
+  <?php } ?>
+<?php } ?>
