df4bc4c269c6fdf8160bcf06d0b63e9149322036
dedit8 <64446967+dedit8@users.noreply.github.com>
2021-08-23 19:14:08 -0300

    Add files via upload
    pedido del navaru

--- semi/navbaru.php
+++ semi/navbaru.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <script src="https://kit.fontawesome.com/5ad54e1f93.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/navaru.css">
   </head>
   <body>
     <header class="header">
@@ -11,10 +12,8 @@
 
           <div class="log_reg">
             <a href="http://localhost/semi/datu">Usuario</a>
-            <a>...</a>
             <a href="http://localhost/semi/pedi">Pedir Semillas</a>
-            <a>...</a>
-            <a class="nav-link" href="https://docs.google.com/forms/d/e/1FAIpQLSd9aNHOmZ2Ufzgu0JAXUzFBvnrtFixAeSVgwncNUzEYFY7Prw/viewform?usp=sf_link">Cuéntanos cómo te fue con la cosecha...</a>
+            <a class="nav-link" href="https://docs.google.com/forms/d/e/1FAIpQLSd9aNHOmZ2Ufzgu0JAXUzFBvnrtFixAeSVgwncNUzEYFY7Prw/viewform?usp=sf_link">Cuéntanos cómo te fue con la cosecha</a>
             <a href="http://localhost/semi/serse">Cerrar Sesión</a>
           </div>
         </nav>
