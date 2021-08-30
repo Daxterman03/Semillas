633c23077df8e854e27dff086d903d2ee0a36f5f
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-08-30 19:05:08 -0300

    Arreglo de navbars

--- semi/navbar2.php
+++ semi/navbar2.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1, user-scalable=no">
+    <title>Programa Cultivar</title>
+    <meta name="description" content="El programa Cultivar acerca a la población una oportunidad de acceso no sólo al recurso sino también al espacio donde se puedan potenciar habilidades, construir vínculos y generar un sustento familiar a través de las huertas.">
+    <link rel="icon" type="image/png" href="css/Logo_Cultivar.png">
+    <link rel="canonical" href="index.php">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet"> 
     <script src="https://kit.fontawesome.com/5ad54e1f93.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/estilos.css">
-    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/nav_pie.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/forms.css">
+  </head>
+  <body>
     <header class="header">
       <nav class="menu">
         <div class="ventanas">
@@ -18,4 +28,5 @@
         </div>
       </nav>
     </header>
-  </head>
+  </body>
+</html>
