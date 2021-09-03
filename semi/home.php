2ed4318b5f60a4af68de729b98a33b93ea12819c
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-09-03 15:53:23 -0300

    Elimine el archivo pedi.php y fue incluido en el home.php

--- semi/home.php
+++ semi/home.php
@@ -14,12 +14,39 @@ if(!isset($dni)){
       <!DOCTYPE html>
       <html lang="en" dir="ltr">
         <head>
-          <meta charset="utf-8">
-          <title></title>
         </head>
         <body>
           <h1>Bienvenido <?php echo $mostrar['nombre']; ?></h1>
+          <h1>Pedidos de Semillas</h1>
+          <div>
+            <div>
+              <br>
+              <table>
+                <tr>
+                  <td>Temporada</td>
+                    <td>Año</td>
+                    <td>Zona</td>
+                    <td>Calle</td>
+                    <td>Día</td>
+                </tr>
+                <?php
+                  $sql="SELECT * from lugares";
+                  $result=mysqli_query($conexion,$sql);
+                  while($mostrar=mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                      echo "<td>"; echo $mostrar['temporada']; echo "</td>";
+                      echo "<td>"; echo $mostrar['ano']; echo "</td>";
+                      echo "<td>"; echo $mostrar['zona']; echo "</td>";
+                      echo "<td>"; echo $mostrar['calle']; echo "</td>";
+                      echo "<td>"; echo $mostrar['dia']; echo "</td>";
+                      echo "<td>  <a href='ped1.php?id=".$mostrar['id']."'> <button type='button' class='button'>pedir</button> </a> </td>";
+                    echo "</tr>";}?>
+              </table>
+            </div>
+          </div>
+            <?php include('pie.php')?>
         </body>
       </html>
         <?php } ?>
 <?php } ?>
+
