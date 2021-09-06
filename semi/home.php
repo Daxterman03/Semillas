a49259f312fd94d72fc120de3405ea35f1a0118f
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-09-06 12:34:40 -0300

    Arreglos en el pedido ya realizado

--- semi/home.php
+++ semi/home.php
@@ -11,42 +11,43 @@ if(!isset($dni)){
     header("location: index.php");
 } else {
 ?>
-      <!DOCTYPE html>
-      <html lang="en" dir="ltr">
-        <head>
-        </head>
-        <body>
-          <div class="cuerpo_ped">
-          <h1>Bienvenido <?php echo $mostrar['nombre']; ?></h1>
-          <h1>Pedidos de Semillas</h1>
-          <?php
-            $sql="SELECT * from lugares";
-            $result=mysqli_query($conexion,$sql);
-            while($mostrar=mysqli_fetch_assoc($result)) {
-              echo '<div class="pedido">';
-                echo '<div class="temporada">';
-                  echo '<h4>Temporada</h4>'; 
-                  echo '<label for="temporada">'.$mostrar['temporada'].'</label>';
-                  echo '<label for="año">'.$mostrar['ano'].'</label>';
-                echo'</div>';
-                echo '<div class="retiro">';
-                  echo '<h5>Se retira el</h5>';
-                  echo '<label for="fecha">'.$mostrar['dia'].'</label>';
-                echo '</div>';
-                echo '<div class="lugar">';
-                  echo '<h5>Calle</h5>';
-                  echo '<label for="calle">'.$mostrar['calle'].'</label>';
-                  echo '<h5>Zona</h5>';
-                  echo '<label for="zona">'.$mostrar['zona'].'</label>';
-                echo '</div>';
-                echo "<a href='ped1.php?id=".$mostrar['id']."'> <button type='button' class='button'>Pedir</button></a>";
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+    </head>
+    <body>
+      <div class="cuerpo_ped">
+        <h1>Bienvenido <?php echo $mostrar['nombre']; ?></h1>
+        <h2>Semillas de temporada</h2>
+        <?php
+          $sql="SELECT * from lugares";
+          $result=mysqli_query($conexion,$sql);
+          while($mostrar=mysqli_fetch_assoc($result)) {
+            echo '<div class="pedido">';
+              echo '<div class="temporada">';
+                echo '<h4>Temporada</h4>'; 
+                echo '<label for="temporada">'.$mostrar['temporada'].'</label>';
+                echo '<br>';
+                echo '<label for="año">'.$mostrar['ano'].'</label>';
+              echo'</div>';
+              echo '<div class="retiro">';
+                echo '<h5>Se retira el</h5>';
+                echo '<label for="fecha">'.$mostrar['dia'].'</label>';
               echo '</div>';
-          }?>
-          <a class="formulario" href="https://docs.google.com/forms/d/e/1FAIpQLSd9aNHOmZ2Ufzgu0JAXUzFBvnrtFixAeSVgwncNUzEYFY7Prw/viewform?usp=sf_link">Cuéntanos cómo te fue con la cosecha</a>
-          </div>
-          <?php include('pie.php')?>
-        </body>
-      </html>
+              echo '<div class="lugar">';
+                echo '<h5>Calle</h5>';
+                echo '<label for="calle">'.$mostrar['calle'].'</label>';
+                echo '<h5>Zona</h5>';
+                echo '<label for="zona">'.$mostrar['zona'].'</label>';
+              echo '</div>';
+              echo "<a href='ped1.php?id=".$mostrar['id']."'> <button type='button' class='button'>Pedir</button></a>";
+            echo '</div>';
+        }?>
+        <a class="formulario" href="https://docs.google.com/forms/d/e/1FAIpQLSd9aNHOmZ2Ufzgu0JAXUzFBvnrtFixAeSVgwncNUzEYFY7Prw/viewform?usp=sf_link">Cuéntanos cómo te fue con la cosecha</a>
+      </div>
+      <?php include('pie.php')?>
+    </body>
+  </html>
         <?php } ?>
 <?php } ?>
 
--- semi/ped1.php
+++ semi/ped1.php
@@ -11,79 +11,59 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
   </head>
-
   <body>
-    <h1>Pedido</h1>
-    <form action="envio.php" method="post">
-      <table>
-        <tr>
-          <td>Temporada</td>
-          <td>
-            <label for="temporada"></label>
-            <h><?php echo $mostrar['temporada']; ?></h>
-            <input type="hidden" name="temporada"value="<?php echo $mostrar['temporada']; ?>" required  />
-          </td>
-        </tr>
-        <tr>
-          <td>Año</td>
-          <td>
-            <label for="ano"></label>
-            <h><?php echo $mostrar['ano']; ?></h>
-            <input type="hidden" name="ano"value="<?php echo $mostrar['ano']; ?>" required  />
-          </td>
-        </tr>
-        <tr>
-          <td>Zona</td>
-          <td>
-            <label for="zona"></label>
-            <h><?php echo $mostrar['zona']; ?></h>
-            <input type="hidden" name="zona"value="<?php echo $mostrar['zona']; ?>" required  />
-          </td>
-        </tr>
-        <tr>
-          <td>Calle</td>
-          <td>
-            <label for="calle"></label>
-            <h><?php echo $mostrar['calle']; ?></h>
-            <input type="hidden" name="calle" value="<?php echo $mostrar['calle']; ?>"required  />
-          </td>
-        </tr>
-        <tr>
-          <td>Día</td>
-          <td>
-            <label for="dia"></label>
-            <h><?php echo $mostrar['dia']; ?></h>
-            <input type="hidden" name="dia"value="<?php echo $mostrar['dia']; ?>" required  />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <label for="id"></label>
-            <input type="hidden" name="id"value="<?php echo $mostrar['id']; ?>" required  />
-          </td>
-        </tr>
-<?php } ?>
-        <tr>
-          <?php
-          $dni = $_SESSION['dni'];
+    <div class="cuerpo_ped">
+      <div class="ped_realiz">
+        <h1>Pedido</h1>
+        <form action="envio.php" method="post">
+          <div class="info_ped">
+            <div class="seccion">
+              <label for="temporada">Temporada</label>
+              <h><?php echo $mostrar['temporada']; ?></h>
+              <input type="hidden" name="temporada"value="<?php echo $mostrar['temporada']; ?>" required  />
+            </div>
+            <div class="seccion">
+              <label for="ano">Año</label>
+              <h><?php echo $mostrar['ano']; ?></h>
+              <input type="hidden" name="ano"value="<?php echo $mostrar['ano']; ?>" required  />
+            </div>
+            <div class="seccion">
+              <label for="zona">Zona</label>
+              <h><?php echo $mostrar['zona']; ?></h>
+              <input type="hidden" name="zona"value="<?php echo $mostrar['zona']; ?>" required  />
+            </div>
+            <div class="seccion">
+              <label for="calle">Calle</label>
+              <h><?php echo $mostrar['calle']; ?></h>
+              <input type="hidden" name="calle" value="<?php echo $mostrar['calle']; ?>"required  />  
+            </div>
+            <div class="seccion">
+              <label for="dia">Día</label>
+              <h><?php echo $mostrar['dia']; ?></h>
+              <input type="hidden" name="dia"value="<?php echo $mostrar['dia']; ?>" required  />
+            </div>
+              <input type="hidden" name="id"value="<?php echo $mostrar['id']; ?>" required  />
+            <?php } ?>
+            <?php
+              $dni = $_SESSION['dni'];
 
-          $sql="SELECT * from usu1 where dni='$dni'";
-          $result=mysqli_query($conexion,$sql);
-          while($mostrar=mysqli_fetch_assoc($result)) {
+              $sql="SELECT * from usu1 where dni='$dni'";
+              $result=mysqli_query($conexion,$sql);
+              while($mostrar=mysqli_fetch_assoc($result)) {
 
-          ?>
-          <td>DNI</td>
-            <td>
-              <label for="dni"></label>
-              <h><?php echo $mostrar['dni']; ?></h>
-              <input type="hidden" name="dni"value="<?php echo $mostrar['dni']; ?>" required  />
-            </td>
-        </tr>
-      </table>
-      <input class="button" type="submit" value="Pedir">
-    </form>
+              ?>
+              <div class="seccion">
+                <label for="dni">DNI</label>
+                <h><?php echo $mostrar['dni']; ?></h>
+                <input type="hidden" name="dni"value="<?php echo $mostrar['dni']; ?>" required  />
+              </div>
+          </div>
+          <input class="button" type="submit" value="Confirmar">
+        </form>
+      </div>
+    </div>
     <?php include('pie.php')?>
+  </body>
 <?php } ?>
 </html>
