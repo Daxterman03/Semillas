658e2b52081b727516db6c6d624c6a498886bf90
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-09-08 17:27:47 -0300

    Arreglos en la parte de contacto del pie de pagina

--- semi/pie.php
+++ semi/pie.php
@@ -4,12 +4,12 @@
     </div>
     <div class="contacto">
         <div class="redes">
-            <a href="mailto:intendencia@venadotuerto.gob.ar"><i class="fas fa-envelope-open-text icon"></i>Mail</a>
-            <a href="https://www.facebook.com/VenadoTuertoGobierno" target="_blank"><i class="fab fa-facebook-square icon"></i>Facebook</a>
-            <a href="https://www.instagram.com/venadotuertogob/?fbclid=IwAR3Hg19tArGL8IMHJNOVwCtQWZAnQ1AQ00eEJfHRU8h79T997rhPRauMUZM" target="_blank"><i class="fab fa-instagram-square icon"></i>Instagram</a>
+            <a href="mailto:intendencia@venadotuerto.gob.ar"><i class="fas fa-envelope-open-text icon"></i></a>
+            <a href="https://www.facebook.com/VenadoTuertoGobierno" target="_blank"><i class="fab fa-facebook-square icon"></i></a>
+            <a href="https://www.instagram.com/venadotuertogob/?fbclid=IwAR3Hg19tArGL8IMHJNOVwCtQWZAnQ1AQ00eEJfHRU8h79T997rhPRauMUZM" target="_blank"><i class="fab fa-instagram-square icon"></i></a>
         </div>
         <div class="celular">
-            <h6><i class="fas fa-phone-square-alt icon"></i>Celular: (03462) 42-1417</h6>
+            <h6><i class="fas fa-phone-square-alt icon icon_"></i>(03462) 42-1417</h6>
         </div>
         <div class="admin">
             <a href="inia.php"><i class="fas fa-users-cog"></i></a>
