9c24f91159240209c0c3c98b6c8f899478b496ee
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-09-13 19:50:23 -0300

    Estilos en la confirmacion del pedido

--- semi/ped1.php
+++ semi/ped1.php
@@ -14,50 +14,51 @@
   </head>
   <body>
     <div class="cuerpo_ped">
-      <div class="ped_realiz">
-        <h1>Pedido</h1>
+      <h1>Pedido</h1>
+      <div class="pedido">
         <form action="envio.php" method="post">
           <div class="info_ped">
-            <div class="seccion">
-              <label for="temporada">Temporada</label>
-              <h><?php echo $mostrar['temporada']; ?></h>
+            <div class="temporada">
+              <h4>Temporada</h4>
+              <label for='temporada'><?php echo $mostrar['temporada']; ?></label>
               <input type="hidden" name="temporada"value="<?php echo $mostrar['temporada']; ?>" required  />
-            </div>
-            <div class="seccion">
-              <label for="ano">Año</label>
-              <h><?php echo $mostrar['ano']; ?></h>
+              <label for='ano'><?php echo $mostrar['ano']; ?></label>
               <input type="hidden" name="ano"value="<?php echo $mostrar['ano']; ?>" required  />
             </div>
-            <div class="seccion">
-              <label for="zona">Zona</label>
-              <h><?php echo $mostrar['zona']; ?></h>
-              <input type="hidden" name="zona"value="<?php echo $mostrar['zona']; ?>" required  />
-            </div>
-            <div class="seccion">
-              <label for="calle">Calle</label>
-              <h><?php echo $mostrar['calle']; ?></h>
-              <input type="hidden" name="calle" value="<?php echo $mostrar['calle']; ?>"required  />  
-            </div>
-            <div class="seccion">
-              <label for="dia">Día</label>
-              <h><?php echo $mostrar['dia']; ?></h>
-              <input type="hidden" name="dia"value="<?php echo $mostrar['dia']; ?>" required  />
-            </div>
+            <div class="centro_pedido">
+              <div class="retiro">
+                <h5>Día</h5>
+                <label><?php echo $mostrar['dia']; ?></label>
+                <input type="hidden" name="dia"value="<?php echo $mostrar['dia']; ?>" required  />
+              </div>
+              <div class="lugar">
+                <div>
+                  <h5>Calle</h5>
+                  <label><?php echo $mostrar['calle']; ?></label>
+                  <input type="hidden" name="calle" value="<?php echo $mostrar['calle']; ?>"required  />  
+                </div>
+                <div>
+                  <h5>Zona</h5>
+                  <label><?php echo $mostrar['zona']; ?></label>
+                  <input type="hidden" name="zona"value="<?php echo $mostrar['zona']; ?>" required  />
+                </div>
+              </div>
               <input type="hidden" name="id"value="<?php echo $mostrar['id']; ?>" required  />
-            <?php } ?>
-            <?php
+              <?php } ?>
+              <?php
               $dni = $_SESSION['dni'];
-
+              
               $sql="SELECT * from usu1 where dni='$dni'";
               $result=mysqli_query($conexion,$sql);
               while($mostrar=mysqli_fetch_assoc($result)) {
-
-              ?>
-              <div class="seccion">
-                <label for="dni">DNI</label>
-                <h><?php echo $mostrar['dni']; ?></h>
+                
+                ?>
+              <div class="pedido_usuario">
+                <h5>DNI</h5>
+                <label><?php echo $mostrar['dni']; ?></label>
                 <input type="hidden" name="dni"value="<?php echo $mostrar['dni']; ?>" required  />
               </div>
+            </div>
           </div>
           <input class="button" type="submit" value="Confirmar">
         </form>
