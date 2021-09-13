f1da22e2337f2414cee7ad1233b5cf05603f6783
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-09-13 13:52:54 -0300

    Arreglos en la parte del pedido de los usuarios

--- semi/home.php
+++ semi/home.php
@@ -19,30 +19,38 @@ if(!isset($dni)){
       <div class="cuerpo_ped">
         <h1>Bienvenido <?php echo $mostrar['nombre']; ?></h1>
         <h2>Semillas de temporada</h2>
-        <?php
-          $sql="SELECT * from lugares";
-          $result=mysqli_query($conexion,$sql);
-          while($mostrar=mysqli_fetch_assoc($result)) {
-            echo '<div class="pedido">';
-              echo '<div class="temporada">';
-                echo '<h4>Temporada</h4>'; 
-                echo '<label for="temporada">'.$mostrar['temporada'].'</label>';
-                echo '<br>';
-                echo '<label for="año">'.$mostrar['ano'].'</label>';
-              echo'</div>';
-              echo '<div class="retiro">';
-                echo '<h5>Se retira el</h5>';
-                echo '<label for="fecha">'.$mostrar['dia'].'</label>';
-              echo '</div>';
-              echo '<div class="lugar">';
-                echo '<h5>Calle</h5>';
-                echo '<label for="calle">'.$mostrar['calle'].'</label>';
-                echo '<h5>Zona</h5>';
-                echo '<label for="zona">'.$mostrar['zona'].'</label>';
-              echo '</div>';
-              echo "<a href='ped1.php?id=".$mostrar['id']."'> <button type='button' class='button'>Pedir</button></a>";
-            echo '</div>';
-        }?>
+        <div class="orden_pedidos">
+          <?php
+            $sql="SELECT * from lugares";
+            $result=mysqli_query($conexion,$sql);
+            while($mostrar=mysqli_fetch_assoc($result)) { ?>
+              <div class="pedido">
+                <div class="temporada">
+                  <h4>Temporada</h4>
+                  <label for="temporada"><?php echo $mostrar['temporada']?></label>
+                  <label for="año"><?php echo $mostrar['ano']?></label>
+                </div>
+                <div class="centro_pedido">
+                  <div class="retiro">
+                    <h5>Se retira el</h5>
+                    <label for="fecha"><?php echo $mostrar['dia']?></label>
+                  </div>
+                  <div class="lugar">
+                    <div>
+                      <h5>Calle</h5>
+                      <label for="calle"><?php echo $mostrar['calle']?></label>
+                    </div>
+                    <div>
+                      <h5>Zona</h5>
+                      <label for="zona"><?php echo $mostrar['zona']?></label>
+                    </div>
+                  </div>
+                </div>
+                <?php echo "<a href='ped1.php?id=".$mostrar['id']."'>" ?>
+                <button type='button' class='button'>Pedir</button></a>
+              </div>
+          <?php }?>
+        </div>
         <a class="formulario" href="https://docs.google.com/forms/d/e/1FAIpQLSd9aNHOmZ2Ufzgu0JAXUzFBvnrtFixAeSVgwncNUzEYFY7Prw/viewform?usp=sf_link">Cuéntanos cómo te fue con la cosecha</a>
       </div>
       <?php include('pie.php')?>
