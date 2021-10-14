75dce01ba8b68f6dc4c1334d87dba3eb0c18081d
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-10-14 10:54:09 -0300

    Verificacion en el pedido manual para no duplicar datos

--- semi/manual_order.php
+++ semi/manual_order.php
@@ -10,10 +10,34 @@ $dni = $_POST['dni'];
 
 $sql = "INSERT INTO pdo (temporada, ano, zona, calle, dia, dni, entregado)
                     values('$temporada', '$ano', '$barrio', '$calle', '$dia', '$dni', 'Si')";
-$accion_insert = mysqli_query($conexion, $sql);
+$verify_pdo = mysqli_query($conexion, "SELECT * FROM pdo WHERE dni = '$dni' and dia = '$dia'");
 
 $sql_new = "INSERT into usu1 (nombre, apellido, mail, telefono, dni, direccion, inte)
                         values ('$nombre', 'NULL', 'Null', 'null', '$dni', 'null', 'null')";
-$accion_insert_new = mysqli_query($conexion, $sql_new);
+$verify_user = mysqli_query($conexion, "SELECT * FROM usu1 Where dni = '$dni'");
 
-echo $temporada, $ano, $dia, $barrio, $calle, $nombre, $dni;
+if (mysqli_num_rows($verify_user) == 0 and mysqli_num_rows($verify_pdo) == 0){
+    $accion_insert = mysqli_query($conexion, $sql);
+    $accion_insert_new = mysqli_query($conexion, $sql_new);
+    echo '
+    <script type="text/javascript">
+    alert("Pedido Registrado y Usuario Registrado");
+    window.location.href="peda.php";
+    </script>
+    ';
+}elseif (mysqli_num_rows($verify_pdo) == 0 and mysqli_num_rows($verify_user) != 0){
+    $accion_insert = mysqli_query($conexion, $sql);
+    echo '
+    <script type="text/javascript">
+    alert("Pedido Registrado y el usuario ya estaba registrado");
+    window.location.href="peda.php";
+    </script>
+    ';
+}else{
+    echo '
+    <script type="text/javascript">
+    alert("El pedido y el usuario ya estaban registrados");
+    window.location.href="peda.php";
+    </script>
+    ';
+}
