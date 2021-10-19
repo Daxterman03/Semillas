0a812bb075ec69236735b44bdb9575d0f3007fcb
Diego Marmiroli <diegomarmiroli20@gmail.com>
2021-10-19 19:44:57 -0300

    Creado el apartado de los desarrolladores

---
+++ semi/developer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desarrolladores</title>
+    <link rel="stylesheet" href="css/ventana.css">
+    <script src="https://kit.fontawesome.com/5ad54e1f93.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php include('index.php')?>
+    <div class="overlay" id="overlay">
+        <div class="desplegable" id="popup">
+            <div>
+                <a href="#" id='btn-cerrar-popup' class="button_dev"><i class="fas fa-times" id='btn-cerrar-popup'></i></a>
+            </div>
+            <div class="personas">
+                <div class="integrantes">
+                    <h1>Diego Marmiroli</h1>
+                    <div class="redes_dev">
+                        <a href="https://www.linkedin.com/in/diego-marmiroli-393255213/"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.instagram.com/diegomarmiroli/?hl=es"><i class="fab fa-instagram"></i></a>
+                        <a href="mailto:diegomarmiroli20@gmail.com"><i class="fas fa-at"></i></a>
+                    </div>
+                </div>
+                <div class="integrantes">
+                    <h1>Nombre</h1>
+                    <div class="redes_dev">
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </div>
+                </div>
+                <div class="integrantes">
+                    <h1>Nombre</h1>
+                    <div class="redes_dev">
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </div>
+                </div>
+                <div class="integrantes">
+                    <h1>Nombre</h1>
+                    <div class="redes_dev">
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </div>
+                </div>
+                <div class="integrantes">
+                    <h1>Nombre</h1>
+                    <div class="redes_dev">
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/popups.js"></script>
+</body>
+</html>
